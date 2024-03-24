c8e65da326a45970f35efebd3cd2ed2db8370809
Davidvera27 <davidrestrepovera@gmail.com>
2024-03-24 18:53:59 -0500

    Update Iniciar_Sesion.py

--- Iniciar_Sesion.py
+++ Iniciar_Sesion.py
@@ -1,45 +1,30 @@
+import time
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
+from PyQt5.QtWidgets import QMessageBox
 
 def iniciar_sesion(usuario, contraseña):
-    # Inicializar el driver del navegador
-    driver = webdriver.Chrome()  # Esto inicializará Chrome
     try:
-        # Navegar a la página de inicio de sesión
+        driver = webdriver.Chrome()
         driver.get("https://radicacion.supernotariado.gov.co/app/inicio.dma")
-        
-        # Esperar a que se cargue la página de inicio de sesión
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "formLogin:usrlogin")))
-        
-        # Ingresar las credenciales en el formulario de inicio de sesión
+
         usr_input = driver.find_element(By.ID, "formLogin:usrlogin")
         usr_input.send_keys(usuario)
-        
+
         pwd_input = driver.find_element(By.ID, "formLogin:j_idt8")
         pwd_input.send_keys(contraseña)
-        
-        # Hacer clic en el botón "Autenticarse"
-        autenticarse_btn = driver.find_element(By.ID, "formLogin:j_idt11")
-        autenticarse_btn.click()
-        
-        # Esperar a que se cargue la página de inicio después del inicio de sesión
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "id_of_an_element_on_the_next_page")))
-        
-        print("Inicio de sesión exitoso.")
-    
-    except Exception as e:
-        print("Error durante el inicio de sesión:", e)
-    
-    finally:
-        # Cerrar el navegador al finalizar
-        driver.quit()
 
-# Ejemplo de uso
-if __name__ == "__main__":
-    usuario = input("Ingresa tu usuario: ")
-    contraseña = input("Ingresa tu contraseña: ")
-    iniciar_sesion(usuario, contraseña)
+        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "formLogin:j_idt11"))).click()
+
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "infoBienvenida")))
+
+        driver.get("https://radicacion.supernotariado.gov.co/app/external/documentary-manager.dma")
+
+        QMessageBox.information(None, "Inicio de Sesión Exitoso", "¡Inicio de sesión exitoso!")
+    except Exception as e:
+        QMessageBox.critical(None, "Error", "Error durante el inicio de sesión: El sitio web no está disponible. Por favor, inténtelo de nuevo más tarde.")
+    finally:
+        if 'driver' in locals():
+            driver.quit()
