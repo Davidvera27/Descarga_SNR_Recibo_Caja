4f508d83b0f2f341369e3a42fac52754c3d2f3f6
Davidvera27 <davidrestrepovera@gmail.com>
2024-03-28 12:25:41 -0500

    Update Descargar_Rentas.py

--- Descargar_Rentas.py
+++ Descargar_Rentas.py
@@ -5,8 +5,8 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from PyQt5 import QtWidgets, QtCore
-from PyQt5.QtGui import QIcon
+from PyQt5 import QtWidgets, QtCore, QtWebEngineWidgets
+
 
 class ConsultaAnexosInterfaz(QtWidgets.QWidget):
     def __init__(self, excel_file_path):
@@ -15,11 +15,21 @@ class ConsultaAnexosInterfaz(QtWidgets.QWidget):
         self.setGeometry(100, 100, 800, 400)
         self.excel_file_path = excel_file_path
         self.proceso_en_curso = False
-        self.icono_busqueda = QIcon("PYTHON\WebScraping\Excel\Descarga_SNR_Recibo_Caja\Icons\Search_icon.png")
-        self.icono_ver = QIcon("PYTHON\WebScraping\Excel\Descarga_SNR_Recibo_Caja\Icons\ver_icon.png")
 
-        # Estilos
-        self.setStyleSheet("background-color: #f0f0f0;")
+        # Cargar los estilos desde el archivo CSS
+        with open("PYTHON\WebScraping\Excel\Descarga_SNR_Recibo_Caja\styles_Interfaz_Rentas.css", "r") as css_file:
+            self.setStyleSheet(css_file.read())
+
+        # Campos de búsqueda
+        self.label_buscar_escritura = QtWidgets.QLabel("Buscar por ESCRITURA:", self)
+        self.line_edit_escritura = QtWidgets.QLineEdit(self)
+        self.label_buscar_escritura.setGeometry(50, 10, 150, 30)
+        self.line_edit_escritura.setGeometry(200, 10, 150, 30)
+
+        self.label_buscar_radicado = QtWidgets.QLabel("Buscar por RADICADO:", self)
+        self.line_edit_radicado = QtWidgets.QLineEdit(self)
+        self.label_buscar_radicado.setGeometry(400, 10, 150, 30)
+        self.line_edit_radicado.setGeometry(550, 10, 150, 30)
 
         # Tabla de anexos
         self.tabla_anexos = QtWidgets.QTableWidget(self)
@@ -27,41 +37,25 @@ class ConsultaAnexosInterfaz(QtWidgets.QWidget):
         self.tabla_anexos.setColumnCount(4)
         self.tabla_anexos.setHorizontalHeaderLabels(["ESCRITURA", "RADICADO", "ESTADO DE LIQUIDACIÓN", "VER"])
         self.tabla_anexos.verticalHeader().setVisible(False)
-        self.tabla_anexos.horizontalHeader().setStyleSheet("QHeaderView::section { background-color: #d3d3d3; }")
-        self.tabla_anexos.setStyleSheet("color: #333; font-size: 12px;")
         self.tabla_anexos.horizontalHeader().setStretchLastSection(True)
 
         # Configurar el botón de consulta
         self.boton_consultar = QtWidgets.QPushButton("ABRIR LIQUIDACIÓN DE RENTAS", self)
         self.boton_consultar.setGeometry(50, 360, 250, 30)
-        self.boton_consultar.setIcon(self.icono_busqueda)  # Asignar el ícono de búsqueda al botón
-        self.boton_consultar.setIconSize(QtCore.QSize(24, 24))  # Ajustar el tamaño del ícono
-        self.boton_consultar.setStyleSheet("QPushButton { background-color: #ADD8E6; color: white; border: 1px solid #4682B4; border-radius: 5px; }"
-                                           "QPushButton:hover { background-color: #87CEEB; }"
-                                           "QPushButton:pressed { background-color: #4682B4; }")  # Establecer estilo CSS
         self.boton_consultar.clicked.connect(self.consultar_anexos)
 
         # Configurar el botón de reiniciar
         self.boton_reiniciar = QtWidgets.QPushButton("REINICIAR", self)
         self.boton_reiniciar.setGeometry(320, 360, 250, 30)
-        self.boton_reiniciar.setIcon(self.icono_busqueda)  # Asignar el ícono de búsqueda al botón
-        self.boton_reiniciar.setIconSize(QtCore.QSize(24, 24))  # Ajustar el tamaño del ícono
-        self.boton_reiniciar.setStyleSheet("QPushButton { background-color: #ADD8E6; color: white; border: 1px solid #4682B4; border-radius: 5px; }"
-                                            "QPushButton:hover { background-color: #87CEEB; }"
-                                            "QPushButton:pressed { background-color: #4682B4; }")  # Establecer estilo CSS
         self.boton_reiniciar.clicked.connect(self.reiniciar_proceso)
 
-        # Otras configuraciones
         self.chrome_options = Options()
         self.chrome_options.add_argument("--headless")
         self.driver = webdriver.Chrome(options=self.chrome_options)
 
-        self.cargar_iconos()
-
-    def cargar_iconos(self):
-        # Aquí cargarías los iconos desde tus archivos locales o descargarías desde internet
-        # Ejemplo de cómo se cargarían los iconos
-        self.icono_ver = QIcon("ver.png")
+        # Conectar los eventos textChanged de los QLineEdit a la función de filtrado
+        self.line_edit_escritura.textChanged.connect(self.filtrar_tabla_por_escritura)
+        self.line_edit_radicado.textChanged.connect(self.filtrar_tabla_por_radicado)
 
     def ver_anexo(self):
         boton = self.sender()
@@ -74,6 +68,17 @@ class ConsultaAnexosInterfaz(QtWidgets.QWidget):
         self.driver = webdriver.Chrome()
         self.driver.get(url_consulta)
 
+        try:
+            # Esperar a que se cargue la tabla de anexos
+            WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, "tr.contenido td a[href*='EmergenteImagen']"))
+            )
+            # Encontrar el enlace dentro de la tabla y hacer clic en él
+            enlace_anexo = self.driver.find_element(By.CSS_SELECTOR, "tr.contenido td a[href*='EmergenteImagen']")
+            enlace_anexo.click()
+        except Exception as e:
+            print(f"Error al abrir el anexo: {e}")
+
     def consultar_anexos(self):
         radicados_a_consultar = self.buscar_nir_para_consulta(self.excel_file_path)
         self.proceso_en_curso = True
@@ -82,7 +87,6 @@ class ConsultaAnexosInterfaz(QtWidgets.QWidget):
             try:
                 estado_liquidacion, enlaces_anexos = self.obtener_estado_y_enlaces_anexos(radicado)
                 if enlaces_anexos:
-                    # Agregar fila a la tabla visible en la interfaz gráfica
                     self.agregar_fila_tabla_anexos(escritura_procesada, radicado, estado_liquidacion)
             except Exception as e:
                 print(f"Error al consultar radicado {radicado}: {e}")
@@ -93,7 +97,18 @@ class ConsultaAnexosInterfaz(QtWidgets.QWidget):
     def buscar_nir_para_consulta(self, excel_file_path):
         wb = openpyxl.load_workbook(excel_file_path, data_only=True)
         sheet = wb["ESCRITURAS FISICAS (DIARIO)"]
-        return [(row[2], row[1]) for row in sheet.iter_rows(min_row=2, max_col=4, max_row=sheet.max_row, values_only=True) if row[3] == "SIN INICIAR" and row[1]]
+
+        # Buscar la última fila con datos en la columna D (columna 4)
+        last_row = sheet.max_row
+        for row in range(last_row, 1, -1):
+            if sheet.cell(row=row, column=4).value == "SIN INICIAR":
+                last_valid_row = row
+                break
+
+        # Obtener los datos de las filas relevantes
+        rows = sheet.iter_rows(min_row=2, max_row=last_valid_row, max_col=4, values_only=True)
+        
+        return [(row[2], row[1]) for row in rows if row[3] == "SIN INICIAR" and row[1]]
 
     def obtener_estado_y_enlaces_anexos(self, radicado):
         url_consulta = f"https://mercurio.antioquia.gov.co/mercurio/servlet/ControllerMercurio?command=anexos&tipoOperacion=abrirLista&idDocumento={radicado}&tipDocumento=R&now=Date()&ventanaEmergente=S&origen=NTR"
@@ -111,23 +126,34 @@ class ConsultaAnexosInterfaz(QtWidgets.QWidget):
         self.tabla_anexos.setItem(fila, 1, QtWidgets.QTableWidgetItem(str(radicado)))
         self.tabla_anexos.setItem(fila, 2, QtWidgets.QTableWidgetItem(estado_liquidacion))
 
-        # Configurar el botón VER en la columna 3
         boton_ver = QtWidgets.QPushButton(self)
-        boton_ver.setText("VER")  # Establecer el texto del botón
-        boton_ver.setIcon(self.icono_ver)  # Asignar el ícono VER al botón
-        boton_ver.setIconSize(QtCore.QSize(24, 24))  # Ajustar el tamaño del ícono
-        boton_ver.setStyleSheet("QPushButton { text-align: left; padding-left: 5px; padding-right: 5px; background-color: #ADD8E6; border: 1px solid #4682B4; border-radius: 5px; }"
-                                "QPushButton:hover { background-color: #87CEEB; }"
-                                "QPushButton:pressed { background-color: #4682B4; }")  # Establecer estilo CSS
+        boton_ver.setText("VER")
         boton_ver.clicked.connect(self.ver_anexo)
-        self.tabla_anexos.setCellWidget(fila, 3, boton_ver)  # Asignar el botón a la celda de la tabla
+        self.tabla_anexos.setCellWidget(fila, 3, boton_ver)
 
     def reiniciar_proceso(self):
-        # Limpiar la tabla
         self.tabla_anexos.clearContents()
         self.tabla_anexos.setRowCount(0)
-        # Volver a consultar anexos
         self.consultar_anexos()
+
+    def filtrar_tabla_por_escritura(self, texto):
+        for fila in range(self.tabla_anexos.rowCount()):
+            item = self.tabla_anexos.item(fila, 0)
+            if item:
+                if texto.lower() in item.text().lower():
+                    self.tabla_anexos.setRowHidden(fila, False)
+                else:
+                    self.tabla_anexos.setRowHidden(fila, True)
+
+    def filtrar_tabla_por_radicado(self, texto):
+        for fila in range(self.tabla_anexos.rowCount()):
+            item = self.tabla_anexos.item(fila, 1)
+            if item:
+                if texto.lower() in item.text().lower():
+                    self.tabla_anexos.setRowHidden(fila, False)
+                else:
+                    self.tabla_anexos.setRowHidden(fila, True)
+
 
 if __name__ == "__main__":
     excel_file_path = r'C:\Users\DAVID\Desktop\DAVID\N-15\DAVID\LIBROS XLSM\HISTORICO.xlsm'
