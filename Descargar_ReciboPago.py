4cdf88c06c5eafe000adfaa5b105c6aa2bdc4650
Davidvera27 <davidrestrepovera@gmail.com>
2024-04-01 19:15:48 -0500

    Update Descargar_ReciboPago.py

--- Descargar_ReciboPago.py
+++ Descargar_ReciboPago.py
@@ -1,14 +1,14 @@
 import sys
+import os
 from selenium.webdriver.common.keys import Keys
-from PyQt5.QtCore import QUrl
-from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget, QHeaderView, QPushButton
+from PyQt5.QtCore import QTimer, QUrl
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget, QTableWidgetItem, QVBoxLayout, QWidget, QPushButton
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from openpyxl import load_workbook
-from selenium.common.exceptions import TimeoutException, WebDriverException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -28,131 +28,109 @@ class MainWindow(QMainWindow):
         self.table_widget.setHorizontalHeaderLabels(["ESCRITURA", "Estado del Recibo", "ABRIR CASO"])
         self.layout.addWidget(self.table_widget)
         self.central_widget.setLayout(self.layout)
-        self.headless = True  # Por defecto, el navegador se ejecutará en modo Headless
 
-        options = Options()
-        options.add_argument("--start-maximized")
-        options.add_argument("--headless")  # Agregar esta línea para ejecutar en modo Headless
-        service = Service(ChromeDriverManager().install())
-        self.driver = webdriver.Chrome(service=service, options=options)
+        # Configuración de Selenium
+        self.options = Options()
+        self.options.add_argument("--start-maximized")
+        self.service = Service(ChromeDriverManager().install())
+        self.driver = None  # Inicializar el driver como None
 
-        # Realizar la consulta inicial sobre todos los casos
-        self.consultar_casos()
-        
         # Cargar estilos CSS personalizados
-        with open("PYTHON/WebScraping/Excel/Descarga_SNR_Recibo_Caja/styles_Interfaz_ReciboPago.css", "r") as f:
+        self.load_custom_css()
+
+        # Iniciar el temporizador para la consulta en segundo plano
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.consultar_casos)
+        self.timer.start(1000)  # Inicia la consulta después de 1 segundo
+
+    def load_custom_css(self):
+        # Cargar el archivo CSS personalizado desde otro módulo
+        css_file_path = os.path.join(os.path.dirname(__file__), "styles_Interfaz_ReciboPago.css")
+        with open(css_file_path, "r") as f:
             self.setStyleSheet(f.read())
 
-        # Mostrar la interfaz con los resultados de la consulta
-        self.mostrar_interfaz()
+    def iniciar_sesion(self):
+        # Abrir una sesión del navegador si no está abierta
+        if self.driver is None:
+            self.driver = webdriver.Chrome(service=self.service, options=self.options)
+            self.driver.get("https://radicacion.supernotariado.gov.co/app/inicio.dma")
+            # Seleccionar la opción "Pagos en línea"
+            self.driver.find_element(By.ID, "formLinks:paymentLink").click()
+            # Esperar a que aparezca el campo de búsqueda NIR
+            WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//input[@id='filtroForm:j_idt41']")))
 
     def consultar_casos(self):
         try:
+            # Iniciar sesión si no está iniciada
+            self.iniciar_sesion()
+        
+            # Ruta del archivo Excel
+            excel_file_path = os.path.join(os.getenv("USERPROFILE"), "Desktop", "DAVID", "N-15", "DAVID", "LIBROS XLSM", "HISTORICO.xlsm")
             # Cargar el archivo Excel y seleccionar la hoja principal
-            wb = load_workbook("C:/Users/DAVID/Desktop/DAVID/N-15/DAVID/LIBROS XLSM/HISTORICO.xlsm", data_only=True)
+            wb = load_workbook(excel_file_path, data_only=True)
             ws = wb["PRINCIPAL"]
 
-            # Iterar sobre las filas y procesar aquellas que cumplan con las condiciones
+            # Consultar casos
             for row in ws.iter_rows(min_row=2, max_col=11):
-                try:
-                    if verificar_condiciones(row):
-                        nir = row[3].value
-                        escritura_numero = row[1].value
-                        self.driver.get("https://radicacion.supernotariado.gov.co/app/inicio.dma")
-                        # Seleccionar la opción "Pagos en linea"
-                        self.driver.find_element(By.ID, "formLinks:paymentLink").click()
+                if verificar_condiciones(row):
+                    nir = row[3].value
+                    escritura_numero = row[1].value
+                    estado_recibo = consultar_nir(self.driver, nir, escritura_numero)
+                    self.mostrar_resultado(escritura_numero, estado_recibo, nir)
 
-                        # Esperar a que aparezca el campo de búsqueda NIR
-                        WebDriverWait(self.driver, 10).until(
-                            EC.visibility_of_element_located((By.ID, "filtroForm:j_idt41"))
-                        )
-                        estado_recibo = consultar_nir(self.driver, nir, escritura_numero)
-                        self.mostrar_resultado(escritura_numero, estado_recibo, nir)
-                except TimeoutException as e:
-                    print(f"Tiempo de espera agotado: {str(e)}")
-                except WebDriverException as e:
-                    print(f"Error de WebDriver: {str(e)}")
+            # Detener el temporizador una vez que todas las filas se han procesado
+            self.timer.stop()
+
         except Exception as e:
             print(f"Error al consultar casos: {str(e)}")
 
-    def mostrar_interfaz(self):
-        self.show()
 
     def mostrar_resultado(self, escritura_numero, estado_recibo, nir):
-        # Obtener el número de filas actual en la tabla
         row_position = self.table_widget.rowCount()
         self.table_widget.insertRow(row_position)
         self.table_widget.setItem(row_position, 0, QTableWidgetItem(str(escritura_numero)))
         self.table_widget.setItem(row_position, 1, QTableWidgetItem(estado_recibo))
-
-        # Crear el botón "ABRIR" y conectarlo con la función abrir_caso
+        
         abrir_button = QPushButton("ABRIR", self)
         abrir_button.clicked.connect(lambda _, nir_value=nir: self.abrir_caso(nir_value))
         self.table_widget.setCellWidget(row_position, 2, abrir_button)
-        abrir_button.clicked.connect(self.mostrar_navegador_normal)
-        
-    def mostrar_navegador_normal(self):
-        # Esta función se llama cuando se presiona el botón "ABRIR"
-        # Cambiar self.headless a False para ejecutar el navegador en modo normal
-        self.headless = False        
 
     def abrir_caso(self, nir):
         try:
-            # Abrir el navegador en modo Headless solo si self.headless es True
-            if self.headless:
-                # Configuración de Selenium en modo Headless
-                options = Options()
-                options.add_argument("--start-maximized")                
-                service = Service(ChromeDriverManager().install())
-                self.driver = webdriver.Chrome(service=service, options=options)
-            else:
-                # Configuración de Selenium en modo normal
-                service = Service(ChromeDriverManager().install())
-                self.driver = webdriver.Chrome(service=service)
-
-            # Navegar a la URL del sitio web
-            self.driver.get("https://radicacion.supernotariado.gov.co/app/inicio.dma")
-
-            # Seleccionar la opción "Pagos en línea"
-            self.driver.find_element(By.ID, "formLinks:paymentLink").click()
-
-            # Esperar a que aparezca el campo de búsqueda NIR
-            WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.ID, "filtroForm:j_idt41")))
-
+            # Iniciar sesión si no está iniciada
+            self.iniciar_sesion()
+            
             # Ingresar el NIR en el campo de texto
-            input_nir = self.driver.find_element(By.ID, "filtroForm:j_idt41")
+            input_nir = self.driver.find_element(By.XPATH, "//input[@id='filtroForm:j_idt41']")
             input_nir.clear()
-            input_nir.send_keys(nir)
-            input_nir.send_keys(Keys.RETURN)  # Presionar Enter para buscar el NIR
+            input_nir.send_keys(nir, Keys.RETURN)  # Presionar Enter para buscar el NIR
+
+            # Esperar a que se carguen los resultados
+            WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.ui-g-12.ui-md-6.ui-gl-6")))
 
         except Exception as e:
             print(f"Error al abrir el caso: {str(e)}")
 
-    def closeEvent(self, event):
-        # Cerrar el navegador al cerrar la ventana principal
-        if self.driver is not None:
-            self.driver.quit()
-        event.accept()  # Aceptar el evento de cierre de la ventana principal
-
 def verificar_condiciones(row):
-    columna_b = row[1].value
-    columna_d = row[3].value
-    columna_k = row[10].value
-    return columna_b and not columna_k and columna_d != "No encontrado" and columna_d is not None
+    columna_d = row[3].value  # Columna D (columna 4)
+    columna_k = row[10].value  # Columna K (columna 11)
+    
+    # Verificar si la columna K está vacía y la columna D no es "No encontrado" ni nula
+    if not columna_k and columna_d != "No encontrado" and columna_d is not None:
+        return True
+    else:
+        return False
+
 
 def consultar_nir(driver, nir, escritura_numero):
     try:
-        input_nir = driver.find_element(By.ID, "filtroForm:j_idt41")
+        input_nir = driver.find_element(By.XPATH, "//input[@id='filtroForm:j_idt41']")
         input_nir.clear()
         input_nir.send_keys(nir)
-        driver.find_element(By.ID, "filtroForm:j_idt43").click()
-
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "div.ui-g-12.ui-md-6.ui-gl-6"))
-        )
+        driver.find_element(By.XPATH, "//button[@id='filtroForm:j_idt43']").click()
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.ui-g-12.ui-md-6.ui-gl-6")))
 
         container = driver.find_element(By.CSS_SELECTOR, "div.ui-g-12.ui-md-6.ui-gl-6")
-
         rechazado_texto = container.find_elements(By.XPATH, "//label[contains(text(), 'Documento en estado Ingreso Rechazado')]")
         
         if rechazado_texto:
@@ -161,7 +139,7 @@ def consultar_nir(driver, nir, escritura_numero):
             return f"Aún no se ha cargado boleta de rentas para el caso."
         elif container.find_elements(By.XPATH, "//label[contains(text(), 'Se deben pagar todos los impuestos de registro para generar el recibo de pago')]") and container.find_elements(By.XPATH, "//label[contains(text(), 'Documento en estado Aprobación Pendiente')]"):
             return f"Caso en estado de aprobación PENDIENTE. Se debe cargar boleta de rentas al caso"
-        elif container.find_elements    (By.XPATH, "//label[contains(text(), 'Se deben pagar todos los impuestos de registro para generar el recibo de pago')]") and container.find_elements(By.XPATH, "//label[contains(text(), 'Documento en estado Aprobación N/A')]"):
+        elif container.find_elements(By.XPATH, "//label[contains(text(), 'Se deben pagar todos los impuestos de registro para generar el recibo de pago')]") and container.find_elements(By.XPATH, "//label[contains(text(), 'Documento en estado Aprobación N/A')]"):
             return f"Caso en estado de aprobación 'N/A'. Se debe cargar boleta de rentas al caso"
         elif container.find_elements(By.XPATH, "//div[contains(text(), 'PAGAR EN LÍNEA')]"):
             return f"Recibo de pago descargado y sin cancelar"
@@ -171,8 +149,6 @@ def consultar_nir(driver, nir, escritura_numero):
             return f"Recibo de pago descargado y cancelado"
         else:
             return f"No se puede determinar el estado del recibo"
-    except TimeoutException:
-        return f"Tiempo de espera agotado"
     except Exception as e:
         return f"Error durante la consulta: {str(e)}"
 
